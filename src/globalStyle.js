c9443613ad6314dea982119f5b57404e87be7567
eosun77 <eosun77>
2023-07-13 12:58:26 +0900

    feat: add code style

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -19,6 +19,27 @@ body {
     font-family: "Pretendard-Regular";
     color: var(--black-color);
 }
+
+code {
+    font-family: SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
+    font-size: 0.875em;
+    color: #d14;
+    background-color: #f7f7f9;
+    border-radius: 3px;
+    padding: 2px 4px;
+}
+
+pre {
+    display: block;
+    padding: 10px;
+    margin: 0;
+    overflow: auto;
+    font-size: 85%;
+    line-height: 1.45;
+    background-color: #f6f8fa;
+    border-radius: 3px;
+}
+
 `;
 
 export default GlobalStyle;
